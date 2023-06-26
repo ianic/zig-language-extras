052f8aa09527e562100051c17923baa516212659
Igor Anić <igor.anic@gmail.com>
2023-06-26 22:41:05 +0200

    add debug test command

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,9 @@
 import * as vscode from 'vscode';
 import * as cp from 'child_process';
 import * as path from 'path';
+import * as fs from 'fs';
 
 import { Diagnostic } from './diagnostic';
-
 
 // globals, set in activate used in other functions
 let diagnosticCollection: vscode.DiagnosticCollection;
@@ -23,6 +23,65 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand('zig-language-extras.runFileTests', runFileTests)
 	);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('zig-language-extras.debugTest', debugTest)
+	);
+}
+
+// path for debug binary relative to the workspace root
+const defaultTestBinaryPath = "./zig-out/debug/test";
+const defaultDebugConfiguration = "ZigDebugTest";
+
+function debugTest() {
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) { return; }
+
+	const cwd = vscode.workspace.getWorkspaceFolder(editor.document.uri)?.uri.fsPath || "";
+	const zigConfig = vscode.workspace.getConfiguration('zig');
+	const zigPath = zigConfig.get<string>("zigPath") || "zig";
+	const fileName = editor.document.fileName;
+	const fileNameRelative = path.relative(cwd, fileName);
+
+	const testName = findTest(editor);
+	if (!testName) {
+		output.appendLine("Test not found.");
+		output.show(true);
+		return;
+	}
+
+	const config = vscode.workspace.getConfiguration('zig-language-extras');
+	const testBinaryPath = config.get<string>("testBinaryPath") || defaultTestBinaryPath;
+	const debugConfiguration = config.get<string>("debugConfiguration") || defaultDebugConfiguration;
+
+	diagnosticCollection.clear();
+	output.clear();
+
+	mkdirp(path.resolve(cwd, testBinaryPath));
+
+	const args: string[] = ["test", "--test-filter", testName, fileNameRelative, "-femit-bin=" + testBinaryPath];
+	output.appendLine("Running: zig test --test-filter '" + testName + "' " + fileNameRelative + " -femit-bin=" + testBinaryPath);
+	output.appendLine(""); // empty line
+	cp.execFile(zigPath, args, { cwd }, (err, stdout, stderr) => {
+		output.appendLine(stderr);
+		if (err) {
+			const diagnostic = new Diagnostic(cwd, stderr);
+			diagnostic.createProblems(diagnosticCollection);
+		} else {
+			output.appendLine("Starting launch configuration '" + debugConfiguration + "'");
+			vscode.debug.startDebugging(vscode.workspace.getWorkspaceFolder(editor.document.uri), debugConfiguration);
+		}
+	});
+	output.show(true);
+}
+
+function mkdirp(filePath: string) {
+	console.log("mkdirp filePath", filePath);
+	var dir = path.dirname(filePath);
+	console.log("mkdirp dir", dir);
+
+	if (!fs.existsSync(dir)) {
+		fs.mkdirSync(dir, { recursive: true });
+	}
 }
 
 function runSingleTest() {
