30dda0a01b3d844bbe90a0b5d52de4c73c207a40
Niakr1s <pavel2188@gmail.com>
2023-10-23 09:33:21 +0300

    fixed path

--- src/extension.ts
+++ src/extension.ts
@@ -210,7 +210,7 @@ function runZig(args: string[], cwd: string, successCallback?: () => void) {
 
 	// get zig binary from zig extension configuration
 	const zigConfig = vscode.workspace.getConfiguration('zig');
-	const zigPath = zigConfig.get<string>("zigPath") || "zig";
+	const zigPath = zigConfig.get<string>("path") || "zig";
 
 	if (args[0] === "test") {
 		// append additional test arguments if set in configuration
